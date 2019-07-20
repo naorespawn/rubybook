2863f0c421a7006b8b28709f5010e53220e0bdbb
Naoki Egawa <nao.respawn@gmail.com>
2019-07-20 19:14:00 +0900

    chapter2 questions

---
+++ chapter_2-3/question6.rb
@@ -0,0 +1,5 @@
+coffee = 300
+espresso = 100
+
+puts "コーヒー:#{coffee}円"
+puts "合計:#{coffee + espresso + espresso}円"
---
+++ chapter_2-3/question7.rb
@@ -0,0 +1,5 @@
+coffee = 400
+espresso = 100
+
+puts "コーヒー:#{coffee}円"
+puts "合計:#{coffee + espresso + espresso}円"
