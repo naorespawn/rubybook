5c0cf166543b8e3aacaeb8d85ce12351ec6bcc83
Naoki Egawa <nao.respawn@gmail.com>
2019-07-27 13:37:53 +0900

    chapter8-1

---
+++ chapter_8/chapter_8-1/class1.rb
@@ -0,0 +1,12 @@
+p 1.class
+p 2.class
+p 100.class
+#=> Integer
+p "カフェラテ".class
+p "hello".class
+p "".class
+#=> String
+p [1, 2, 3].class
+p ["コーヒー","カフェラテ"].class
+p [].class
+#=> Array
---
+++ chapter_8/chapter_8-1/class5.rb
@@ -0,0 +1,6 @@
+p String.new #=> ""
+p String.new("カフェラテ") #=> "カフェラテ"
+p Array.new #=> []
+p Array.new(2, "カフェラテ") #=> ["カフェラテ", "カフェラテ"]
+
+# IntegerクラスにはNewメソッドがない
